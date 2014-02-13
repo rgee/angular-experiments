91f38a25635fec7ab428fdccf95920358183a204
Ryan Gee <ryan.cv.gee@gmail.com>
2014-02-12 21:26:40 -0500

    Space out the todo list

--- todo/app/scripts/directives/addtodobutton.js
+++ todo/app/scripts/directives/addtodobutton.js
@@ -11,6 +11,7 @@ angular.module('todoApp')
         console.log(scope);
         element.bind('click', function() {
           Todos.addTodo(angular.copy(scope.todo));
+          scope.todo = {};
         });
       }
     };
